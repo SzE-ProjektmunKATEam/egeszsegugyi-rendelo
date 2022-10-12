1de4ee787fe6e904447c10a679ee5336e72664c2
Gergő Petrovics <pgddedi2@gmail.com>
2022-10-12 12:06:42 +0200

    added form

--- index.php
+++ index.php
@@ -14,8 +14,9 @@
     <div class="container-md">
       <div id="navPlaceholder"></div>
     </div>
-    <div id="hero" style="height: 50vh;">
-    </div>
+    <div id="hero" style="height: 50vh;"></div>
+    <div id="section-1"></div>
+    <div id="form" style="margin-bottom: 100px;"></div>
   </div>
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
--- js/index.js
+++ js/index.js
@@ -2,5 +2,7 @@ $(function(){
     $('document').ready(function() {
         $("#navPlaceholder").load("./partials/navigation.html");
         $("#hero").load("./partials/hero.html");
+        $("#section-1").load("./partials/section-1.html");
+        $("#form").load("./partials/form.html");
     })
   });
